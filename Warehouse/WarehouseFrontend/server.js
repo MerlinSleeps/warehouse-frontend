116de749aba7d645f90f53195f7d6d524c8d378b
MerlinSleeps <merlin199919@gmail.com>
2020-04-24 12:26:00 +0200

    packageJson

--- Warehouse/WarehouseFrontend/server.js
+++ Warehouse/WarehouseFrontend/server.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(`${__dirname}/dist/ShopFrontend`));
+app.use(express.static(`${__dirname}/dist/WarehouseFrontend`));
 
 app.get('/*', (req, res) => {
-  res.sendFile(`${__dirname}/dist/ShopFrontend/index.html`);
+  res.sendFile(`${__dirname}/dist/WarehouseFrontend/index.html`);
 });
 
 // Start the app
-app.listen(process.env.PORT || 9090);
+app.listen(process.env.PORT || 8080);
